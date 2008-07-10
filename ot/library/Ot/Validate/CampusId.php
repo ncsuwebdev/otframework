e4a49c4104d9242bca20e8a8ae98706c200ffce7
Jason Austin <jfaustin@gmail.com>
2008-07-10 18:53:28 +0000

    fixed validator for unity id's and added validator for campus ids

--- ot/library/Ot/Validate/CampusId.php
+++ ot/library/Ot/Validate/CampusId.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Cyclone
- *
  * LICENSE
  *
  * This source file is subject to the new BSD license that is bundled
@@ -14,26 +12,22 @@
  * obtain it through the world-wide-web, please send an email
  * to itappdev@ncsu.edu so we can send you a copy immediately.
  *
- * @package    Cyclone
- * @subpackage Internal_Validate_UserId
+ * @package    Ot_Validate_CampusId
  * @category   vaidator
  * @copyright  Copyright (c) 2007 NC State University Information Technology Division
  * @license    http://itdapps.ncsu.edu/bsd.txt  BSD License
- * @author     Jason Austin <jason_austin@ncsu.edu>
- * @author     Garrison Locke <garrison_locke@ncsu.edu>
  * @see        http://itdapps.ncsu.edu
  * @version    SVN: $Id: UserId.php 158 2007-07-20 13:44:00Z jfaustin@EOS.NCSU.EDU $
  */
 
 /**
- * validates an NCSU unity ID
+ * validates an NCSU campus ID
  *
- * @package    Cyclone
- * @subpackage Internal_Validate_UserId
+ * @package    Ot_Validate_CampusId
  * @category   vaidator
  * @copyright  Copyright (c) 2007 NC State University Information Technology Division
  */
-class Ot_Validate_UserId implements Zend_Validate_Interface
+class Ot_Validate_CampusId implements Zend_Validate_Interface
 {
     /**
      * error messages
@@ -50,10 +44,10 @@ class Ot_Validate_UserId implements Zend_Validate_Interface
      */
     public function isValid($value)
     {
-        $unityId = preg_match('/^[a-z]{1}[a-z0-9]{2,7}$/', $value);
+        $campusId = preg_match('/^[0-9]{9}$/', $value);
 
-        if (!$unityId) {
-            $this->_messages[] = "User ID is not a valid unity ID.";
+        if (!$campusId) {
+            $this->_messages[] = "Campus ID is not a valid NCSU Campus ID.";
             return false;
         }
 
