9c169697161ab070fa5a294ae6a4f96dd6c6f5ab
Garrison Locke <gplocke@gmail.com>
2009-03-17 18:46:56 +0000

    Fixed a bug with caching

---
+++ 2.0.0/ot/application/models/Ot/Account.php
@@ -0,0 +1,268 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file _LICENSE.txt.
+ *
+ * This license is also available via the world-wide-web at
+ * http://itdapps.ncsu.edu/bsd.txt
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to itappdev@ncsu.edu so we can send you a copy immediately.
+ *
+ * @package    Ot_Account
+ * @category   Model
+ * @copyright  Copyright (c) 2007 NC State University Office of Information Technology
+ * @license    BSD License
+ * @version    SVN: $Id: $
+ */
+
+/**
+ * Model to interact with user profiles
+ *
+ * @package    Ot_Account
+ * @category   Model
+ * @copyright  Copyright (c) 2007 NC State University Office of Information Technology
+ *
+ */
+class Ot_Account extends Ot_Db_Table
+{
+    /**
+     * Name of the table in the database
+     *
+     * @var string
+     */
+    protected $_name = 'tbl_ot_account';
+    
+    /**
+     * The minimum length for a password
+     */
+    protected $_minPasswordLength = 6;
+    
+    /**
+     * The maximum length for a generated password
+     */
+    protected $_maxPasswordLength = 20;
+
+    /**
+     * Primary key of the table
+     *
+     * @var string
+     */
+    protected $_primary = 'accountId';
+    
+    public function getAccount($username, $realm)
+    {
+    	$where = $this->getAdapter()->quoteInto('username = ?', $username)
+    	       . ' AND '
+    	       . $this->getAdapter()->quoteInto('realm = ?', $realm);
+    	       
+    	$result = $this->fetchAll($where);
+    	
+    	if ($result->count() != 1) {
+    		return null;
+    	}
+    	
+    	return $result->current();
+    }
+    
+    public function generatePassword()
+    {
+    	return substr(md5(microtime()), 2, 2 + $this->_minPasswordLength);
+    }
+    
+    public function generateApiCode()
+    {
+        return md5(microtime() * 34);
+    }
+
+    public function verify($accessCode)
+    {
+    	$where = $this->getAdapter()->quoteInto('code = ?', $accessCode);
+    	$this->_messages[] = $where;
+    	$result = $this->fetchAll($where, null, 1);
+    	
+    	if ($result->count() != 1) {
+    		throw new Exception('Code not found');
+    	}
+    	
+    	$this->_apiCodeUser = $result->current();
+    }    
+    
+    public function form($default = array(), $signup = false) 
+    {
+    	$config = Zend_Registry::get('config');
+    	$acl    = Zend_Registry::get('acl');
+    	
+        $form = new Zend_Form();
+        $form->setAttrib('id', 'account')
+	         ->setDecorators(array(
+	                 'FormElements',
+	                 array('HtmlTag', array('tag' => 'div', 'class' => 'zend_form')),
+	                 'Form',
+	         ));
+	    
+        $adapters = $config->app->authentication->toArray();
+        
+        // Realm Select box
+        $realmSelect = $form->createElement('select', 'realm', array('label' => 'Login Method'));
+        foreach ($adapters as $key => $value) {
+            $realmSelect->addMultiOption($key, $value['name']);
+        }
+        $realmSelect->setValue((isset($default['realm'])) ? $default['realm'] : '');         
+        
+        // Create and configure username element:
+        $username = $form->createElement('text', 'username', array('label' => 'model-account-username'));
+        $username->setRequired(true)
+                 ->addFilter('StringTrim')
+                 ->addFilter('Alnum')
+                 ->addFilter('StripTags')
+                 ->setAttrib('maxlength', '64')
+                 ->setValue((isset($default['username'])) ? $default['username'] : ''); 
+
+        // First Name
+        $firstName = $form->createElement('text', 'firstName', array('label' => 'model-account-firstName'));
+        $firstName->setRequired(true)
+                  ->addFilter('StringToLower')
+                  ->addFilter('StringTrim')
+                  ->addFilter('StripTags')
+                  ->addFilter(new Ot_Filter_Ucwords())
+                  ->setAttrib('maxlength', '64')
+                  ->setValue((isset($default['firstName'])) ? $default['firstName'] : '');
+
+        // Last Name
+        $lastName = $form->createElement('text', 'lastName', array('label' => 'model-account-lastName'));
+        $lastName->setRequired(true)
+                 ->addFilter('StringTrim')
+                 ->addFilter('StringToLower')
+                 ->addFilter('StripTags')
+                  ->addFilter(new Ot_Filter_Ucwords())
+                 ->setAttrib('maxlength', '64')
+                 ->setValue((isset($default['lastName'])) ? $default['lastName'] : '');
+
+        // Password field
+		$password = $form->createElement('password', 'password', array('label' => 'model-account-password'));
+        $password->setRequired(true)
+                 ->addValidator('StringLength', false, array($this->_minPasswordLength, $this->_maxPasswordLength))
+                 ->addFilter('StringTrim')
+                 ->addFilter('StripTags');   
+
+        // password confirmation field
+        $passwordConf = $form->createElement('password', 'passwordConf', array('label' => 'model-account-passwordConf'));
+        $passwordConf->setRequired(true)
+                     ->addValidator('StringLength', false, array($this->_minPasswordLength, $this->_maxPasswordLength))
+                     ->addFilter('StringTrim')
+                     ->addFilter('StripTags'); 
+                                      
+        // Email address field
+        $email = $form->createElement('text', 'emailAddress', array('label' => 'model-account-emailAddress'));
+        $email->setRequired(true)
+              ->addFilter('StringTrim')
+              ->addValidator('EmailAddress')
+              ->setValue((isset($default['emailAddress'])) ? $default['emailAddress'] : '');
+        
+        $timezone = $form->createElement('select', 'timezone', array('label' => 'model-account-timezone'));
+        $timezone->addMultiOptions(Ot_Timezone::getTimezoneList());
+        $timezone->setValue((isset($default['timezone']) && $default['timezone'] != '') ? $default['timezone'] : date_default_timezone_get()); 
+        
+        // Role select box
+        $roleSelect = $form->createElement('select', 'role', array('label' => 'model-account-role'));
+        $roleSelect->setRequired(true);
+        $roleSelect->addMultiOption('', '-- Choose Access Role --');
+    
+        $roles = $acl->getAvailableRoles();     
+        foreach ($roles as $r) {
+            $roleSelect->addMultiOption($r['roleId'], $r['name']);
+        }
+        $roleSelect->setValue((isset($default['role'])) ? $default['role'] : '');
+        
+        if ($signup) {
+        	$form->addElements(array($username, $password, $passwordConf, $firstName, $lastName, $email, $timezone));
+        } else {
+        	$me = false;
+        	
+        	if (isset($default['accountId']) && $default['accountId'] == Zend_Auth::getInstance()->getIdentity()->accountId) {
+        		$me = true;
+        	}
+        	
+        	if (!$me) {
+        		$form->addElements(array($realmSelect, $username));
+        	}
+        	
+        	$form->addElements(array($firstName, $lastName, $email, $timezone));
+        	
+        	if (!$me) {
+        		$form->addElement($roleSelect);
+        	}
+        }
+        
+        if (isset($config->app->accountPlugin)) {
+            $acctPlugin = new $config->app->accountPlugin;
+            
+            if (isset($default['accountId'])) {
+            	$subform = $acctPlugin->editSubForm($default['accountId']);
+            } else {
+            	$subform = $acctPlugin->addSubForm();
+            }
+            
+            foreach ($subform->getElements() as $e) {
+                $form->addElement($e);
+            }
+        }
+        
+        $custom = new Ot_Custom();
+        
+        if (isset($default['accountId'])) {
+        	$attributes = $custom->getData('Ot_Profile', $default['accountId'], 'Zend_Form');
+        } else {
+        	$attributes = $custom->getAttributesForObject('Ot_Profile', 'Zend_Form');
+        }
+        
+        foreach ($attributes as $a) {
+            $form->addElement($a['formRender']);
+        }
+                
+        $submit = $form->createElement('submit', 'submit', array('label' => 'form-button-save'));
+        $submit->setDecorators(array(
+                   array('ViewHelper', array('helper' => 'formSubmit'))
+                 ));
+        
+        $cancel = $form->createElement('button', 'cancel', array('label' => 'form-button-cancel'));
+        $cancel->setDecorators(array(
+                   array('ViewHelper', array('helper' => 'formButton'))
+                ));
+                        
+        $form->setElementDecorators(array(
+                  'ViewHelper',
+                  'Errors',      
+                  array('HtmlTag', array('tag' => 'div', 'class' => 'elm')), 
+                  array('Label', array('tag' => 'span'))      
+              ))
+              ->addElements(array($submit, $cancel));
+              
+    	if (isset($default['accountId'])) {
+	    	$accountId = $form->createElement('hidden', 'accountId');
+	    	$accountId->setValue($default['accountId']);
+            $accountId->setDecorators(array(
+                array('ViewHelper', array('helper' => 'formHidden'))
+            )); 
+            
+            $form->addElement($accountId);
+	    }     
+
+	    if ($signup) {
+	        // Realm hidden box
+	        $realmHidden = $form->createElement('hidden', 'realm');
+	        $realmHidden->setValue($default['realm']);
+	        $realmHidden->setDecorators(array(
+	            array('ViewHelper', array('helper' => 'formHidden'))
+	        ));  	    	
+	        
+	    	$form->addElement($realmHidden);
+	    }
+                      
+    	return $form;
+    }
+}
---
+++ 2.0.0/ot/application/modules/admin/controllers/TriggerController.php
@@ -0,0 +1,362 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file _LICENSE.txt.
+ *
+ * This license is also available via the world-wide-web at
+ * http://itdapps.ncsu.edu/bsd.txt
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to itappdev@ncsu.edu so we can send you a copy immediately.
+ *
+ * @package    Admin_TriggerController
+ * @category   Controller
+ * @copyright  Copyright (c) 2007 NC State University Office of Information Technology
+ * @license    BSD License
+ * @version    SVN: $Id: $
+ */
+
+/**
+ * Manages the triggers that are dispatched from the application based on an event.
+ *
+ * @package    Admin_TriggerController
+ * @category   Controller
+ * @copyright  Copyright (c) 2007 NC State University Office of Information Technology
+ */
+class Admin_TriggerController extends Zend_Controller_Action  
+{	
+	/**
+	 * Trigger config file
+	 *
+	 * @var Zend_Config
+	 */
+	protected $_triggerConfig = '';
+	
+	/**
+	 * Setup controller vars
+	 *
+	 */
+	public function init()
+	{
+		$config = Zend_Registry::get('config');
+        $this->_triggerConfig = $config->triggers->trigger;
+        
+        parent::init();
+	}
+	
+    /**
+     * Shows all availabe triggers
+     */
+    public function indexAction()
+    {
+        $this->_helper->pageTitle('admin-trigger-index:title');
+        
+        $this->view->acl = array(
+            'details'   => $this->_helper->hasAccess('details')
+            );
+        
+        $this->view->triggers = $this->_triggerConfig;
+    }
+    
+    /**
+     * Shows the actions for the selected trigger
+     *
+     */
+    public function detailsAction()
+    {
+        $this->view->acl = array(
+            'index'  => $this->_helper->hasAccess('index'),
+            'add'    => $this->_helper->hasAccess('add'),
+            'edit'   => $this->_helper->hasAccess('edit'),
+            'delete' => $this->_helper->hasAccess('delete')
+            );        
+        
+        $get = Zend_Registry::get('getFilter');
+        
+        if (!isset($get->triggerId)) {
+        	throw new Ot_Exception_Input('msg-error-triggerIdNotFound');
+        }
+        
+        $thisTrigger = null;
+        foreach ($this->_triggerConfig as $t) {
+            if ($t->name == $get->triggerId) {
+                $thisTrigger = $t;
+                break;
+            }
+        }
+        
+        if (is_null($thisTrigger)) {
+            throw new Ot_Exception_Data('msg-error-noTrigger');
+        }
+        
+        $this->view->triggerId = $get->triggerId;
+        $this->view->trigger = $thisTrigger;
+        $this->_helper->pageTitle('admin-trigger-details:title', $get->triggerId);
+        
+        $action = new Ot_Trigger_Action();
+
+        $where = $action->getAdapter()->quoteInto('triggerId = ?', $get->triggerId);
+        $actions = $action->fetchAll($where)->toArray();
+        
+        $config = Zend_Registry::get('config');
+        
+        foreach ($actions as &$a) {
+        	$a['helper'] = $config->app->triggerPlugins->{$a['helper']};
+        }
+        
+        $this->view->actions = $actions;
+        
+        $this->view->messages = $this->_helper->flashMessenger->getMessages();
+    }
+    
+    /**
+     * Add a new action to the trigger
+     *
+     */
+    public function addAction()
+    {
+        $get = Zend_Registry::get('getFilter');
+        
+        if (!isset($get->triggerId)) {
+            throw new Ot_Exception_Input('msg-error-triggerIdNotFound');
+        }
+        
+        $thisTrigger = null;
+        foreach ($this->_triggerConfig as $t) {
+            if ($t->name == $get->triggerId) {
+                $thisTrigger = $t;
+                break;
+            }
+        }
+        
+        if (is_null($thisTrigger)) {
+            throw new Ot_Exception_Data('msg-error-noTrigger');
+        }
+        
+        $this->view->triggerId = $get->triggerId;
+        $this->view->trigger = $thisTrigger;
+        $this->_helper->pageTitle('admin-trigger-add:title');
+        
+        $action = new Ot_Trigger_Action();
+
+        $values = array('triggerId' => $get->triggerId);
+        
+        if (isset($get->helper)) {
+            $values['helper'] = $get->helper;
+        }
+        
+        $form = $action->form($values);
+             
+        $messages = array();
+        if ($this->_request->isPost()) {
+        	if ($form->isValid($_POST)) {
+        		$action = new Ot_Trigger_Action();
+        		
+        		$data = array(
+        		  'triggerId' => $get->triggerId,
+        		  'name'      => $form->getValue('name'),
+        		  'helper'    => $form->getValue('helper'),
+        		);
+        		
+        		$triggerActionId = $action->insert($data);
+        		
+        	    $subForm = $form->getSubForm($form->getValue('helper'));
+                
+                $elements = $subForm->getElements();
+                
+                $subData = array();
+                foreach ($elements as $key => $value) {
+                    $subData[$key] = $subForm->getValue($key);
+                }
+                $subData['triggerActionId'] = $triggerActionId;                
+        		
+                $obj = $form->getValue('helper');
+                $thisHelper = new $obj;
+                
+        		$thisHelper->addProcess($subData);
+        		
+        		$logOptions = array(
+                        'attributeName' => 'triggerActionId',
+                        'attributeId'   => $triggerActionId,
+                    );
+                    
+                $this->_helper->log(Zend_Log::INFO, 'Trigger Action added', $logOptions);
+        		
+        		$this->_helper->flashMessenger->addMessage('msg-info-triggerAdded');
+        		
+        		$this->_helper->redirector->gotoUrl('/admin/trigger/details/?triggerId=' . $get->triggerId);
+        		
+        	} else {
+        		$messages[] = 'msg-error-formError';
+        	}
+        }
+        
+        $vars = array();
+               
+        foreach ($thisTrigger->var as $var) {
+            $vars[$var->name] = $var->description;
+        }
+                
+        $this->view->messages = $messages;
+        $this->view->templateVars = $vars;        
+        $this->view->form = $form;
+    }
+
+    /**
+     * Edit an existing action of a trigger
+     *
+     */
+    public function editAction()
+    {       
+        $get = Zend_Registry::get('getFilter');
+        
+        if (!isset($get->triggerActionId)) {
+            throw new Ot_Exception_Input('msg-error-triggerActionIdNotFound');
+        }
+        
+        $action = new Ot_Trigger_Action();
+        
+        $thisAction = $action->find($get->triggerActionId);
+        
+        if (is_null($thisAction)) {
+        	throw new Ot_Exception_Data('msg-error-noTriggerActionId');
+        }
+        
+        $triggerId = $thisAction->triggerId;
+         
+        $thisTrigger = null;
+        foreach ($this->_triggerConfig as $t) {
+            if ($t->name == $triggerId) {
+                $thisTrigger = $t;
+                break;
+            }
+        }
+        
+        if (is_null($thisTrigger)) {
+            throw new Ot_Exception_Data('msg-error-noTrigger');
+        }
+       
+        $this->view->trigger = $thisTrigger;
+        
+        $this->_helper->pageTitle('admin-trigger-edit:title');
+
+        $config = Zend_Registry::get('config');
+        
+        if (!isset($config->app->triggerPlugins->{$thisAction->helper})) {
+            throw new Ot_Exception_Data('msg-error-triggerHelperNotFound');
+        }
+        
+        $values = array('triggerActionId' => $get->triggerActionId);
+        $values = array_merge($values, $thisAction->toArray());
+        
+        $form = $action->form($values);      
+             
+        $messages = array();
+        if ($this->_request->isPost()) {
+            if ($form->isValid($_POST)) {
+                
+                $data = array(
+                  'triggerActionId' => $form->getValue('triggerActionId'),
+                  'name'            => $form->getValue('name'),
+                );
+                
+                $action->update($data, null);
+
+                $subForm = $form->getSubForm($form->getValue('helper'));
+                
+                $elements = $subForm->getElements();
+                
+                $subData = array();
+                foreach ($elements as $key => $value) {
+                    $subData[$key] = $subForm->getValue($key);
+                }
+                $subData['triggerActionId'] = $form->getValue('triggerActionId');
+                
+                $obj = $form->getValue('helper');
+                $thisHelper = new $obj;               
+                
+                $thisHelper->editProcess($subData);
+                
+                $logOptions = array(
+                       'attributeName' => 'triggerActionId',
+                       'attributeId'   => $get->triggerActionId,
+                );
+                    
+                $this->_helper->log(Zend_Log::INFO, 'Trigger Action modified', $logOptions);
+            
+                $this->_helper->flashMessenger->addMessage('msg-info-triggerUpdated');
+                
+                $this->_helper->redirector->gotoUrl('/admin/trigger/details/?triggerId=' . $triggerId);
+                
+            } else {
+                $messages[] = 'msg-error-formError';
+            }
+        }
+        
+        $vars = array();
+        foreach ($thisTrigger->var as $var) {
+            $vars[$var->name] = $var->description;
+        }
+        
+        $this->view->messages = $messages;
+        $this->view->templateVars = $vars;        
+        $this->view->form = $form;
+    }
+    
+    /**
+     * delete an existing trigger action
+     *
+     */
+    public function deleteAction()
+    {
+        $get = Zend_Registry::get('getFilter');
+        
+        if (!isset($get->triggerActionId)) {
+            throw new Ot_Exception_Input('msg-error-triggerActionIdNotFound');
+        }
+        
+        $action = new Ot_Trigger_Action();
+        
+        $thisAction = $action->find($get->triggerActionId);
+        
+        if (is_null($thisAction)) {
+            throw new Ot_Exception_Data('msg-error-noTriggerActionId');
+        }
+            	
+        $triggerId = $thisAction->triggerId;
+        
+        $form = Ot_Form_Template::delete('deleteTrigger');     	
+        
+        if ($this->_request->isPost() && $form->isValid($_POST)) {
+        	
+        	$where = $action->getAdapter()->quoteInto('triggerActionId = ?', $get->triggerActionId);
+        	
+        	$action->delete($where);
+        	
+        	$obj = $thisAction->helper;
+        	
+        	$thisHelper = new $obj;
+        	
+        	$thisHelper->deleteProcess($get->triggerActionId);
+        	
+        	$logOptions = array(
+                       'attributeName' => 'triggerActionId',
+                       'attributeId'   => $get->triggerActionId,
+            );
+                    
+            $this->_helper->log(Zend_Log::INFO, 'Trigger Action deleted', $logOptions);
+        
+            $this->_helper->flashMessenger->addMessage('msg-info-triggerDeleted');
+            
+            $this->_helper->redirector->gotoUrl('/admin/trigger/details/?triggerId=' . $triggerId);
+        }
+        
+        $this->view->form = $form;
+        $this->view->action = $thisAction->toArray();
+        $this->view->triggerId = $triggerId;
+        $this->_helper->pageTitle('admin-trigger-delete:title');
+    }
+}
