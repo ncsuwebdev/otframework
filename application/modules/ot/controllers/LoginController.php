1c7f833725edb58700f49c1880e6865792f11c84
Jason Austin <jfaustin@gmail.com>
2010-01-05 19:26:33 +0000

    fixed bug in login controller and added correct entries to the config file for staging

--- application/modules/ot/controllers/LoginController.php
+++ application/modules/ot/controllers/LoginController.php
@@ -189,7 +189,7 @@ class Ot_LoginController extends Zend_Controller_Action
             	$thisAccount = $account->getAccount($username, $realm);
             	
             	if (is_null($thisAccount)) {
-            		$password = $account->generatePassword();
+            		$password = $account->geneatePassword();
             		
             		$acctData = array(
 	            		'username'  => $username,
@@ -261,7 +261,7 @@ class Ot_LoginController extends Zend_Controller_Action
         	$method = array_pop($loginForms);
         	
             if ($method['autoLogin']) {
-            	$authRealm->realm = $key;
+            	$authRealm->realm = $method['realm'];
                 $authRealm->autoLogin = true;
             
                 $this->_helper->redirector->gotoRoute(array('realm' => $authRealm->realm), 'login', true);
