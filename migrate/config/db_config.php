eec6c3ffe362b33ce1ec37cad2b24681c75b0761
Garrison Locke <gplocke@gmail.com>
2010-01-27 15:57:16 +0000

    Added the inital library for the database migrations piece.

---
+++ migrate/config/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+$db_config = (object) array();
+$db_config->host = '127.0.0.1';
+$db_config->port = '3306';
+$db_config->user = 'local';
+$db_config->pass = 'local';
+$db_config->name = 'test';
+$db_config->db_path = '../db/';
+$db_config->method = 1;
+$db_config->prefix = '';
