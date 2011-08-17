7f7f561e6a90149587cac38aaeac0dbe8984295f
Jason Austin <jfaustin@gmail.com>
2011-08-17 13:45:31 +0000

    Applied the theme enhancements from web provisioning to the OIT theme.

--- public/themes/ot/oit/public/scripts/default.js
+++ public/themes/ot/oit/public/scripts/default.js
@@ -4,6 +4,20 @@ $('document').ready(function() {
         
     baseUrl = $('#baseUrl').val();
 
+    var showSystemMessage = function(messages) {
+        $messages = $('#systemMessages').find('ul').empty();
+
+        if (typeof messages == 'string') {
+            messages = [messages];
+        }
+
+        for (m in messages) {
+            $messages.append($('<li></li>').text(messages[m]));
+        }
+
+        $('#systemMessages').slideDown();
+    };
+
     $('ul.sf-menu').supersubs({ 
         minWidth:    12,   // minimum width of sub-menus in em units 
         maxWidth:    27,   // maximum width of sub-menus in em units 
