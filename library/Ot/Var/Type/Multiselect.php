8dbaa5a033e8661270b66e558154d98c2902c4ed
Jason Austin <jfaustin@gmail.com>
2012-07-18 19:58:23 +0000

    Fixed capitalization issue with mutliselect boxes

--- library/Ot/Var/Type/Multiselect.php
+++ library/Ot/Var/Type/Multiselect.php
@@ -1,9 +1,9 @@
 <?php
-class Ot_Var_Type_MultiSelect extends Ot_Var_Abstract
+class Ot_Var_Type_Multiselect extends Ot_Var_Abstract
 {
     public function renderFormElement()
     {        
-        $elm = new Zend_Form_Element_MultiSelect('config_' . $this->getName(), array('label' => $this->getLabel() . ':'));
+        $elm = new Zend_Form_Element_Multiselect('config_' . $this->getName(), array('label' => $this->getLabel() . ':'));
         $elm->setDescription($this->getDescription());
         $elm->setMultiOptions($this->getOptions());
         $elm->setValue($this->getValue());        
