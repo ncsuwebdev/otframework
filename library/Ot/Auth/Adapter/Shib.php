a28597583d2a09eeaf4635e888c296fa2d51bc88
Lark Mullins <larkmullins@gmail.com>
2016-05-11 14:43:43 -0400

    Finished out Ot Auth Shib Adapter

--- library/Ot/Auth/Adapter/Shib.php
+++ library/Ot/Auth/Adapter/Shib.php
@@ -31,4 +31,82 @@
  * @copyright  Copyright (c) 2007 NC State University Office of
  *             Information Technology
  */
-class Ot_Auth_Adapter_Shib extends \NCSU\Auth\Adapter\ShibAuthAdapter {}
+
+use NCSU\Auth\AuthService,
+    NCSU\Auth\Http\Request,
+    NCSU\Auth\Adapter\ShibAuthAdapter;
+
+class Ot_Auth_Adapter_Shib implements Zend_Auth_Adapter_Interface, Ot_Auth_Adapter_Interface
+{
+    /**
+     * Authenticates the user passed by the constructor, however in this case we
+     * user the Shib server variable "UNITY USERID" to get this appropriate username.
+     *
+     * @return new Zend_Auth_Result object
+     */
+    public function authenticate()
+    {
+        $request = Request::createFromGlobals();
+
+        $shibAuthAdapter = new ShibAuthAdapter($request);
+        $service = new AuthService($shibAuthAdapter);
+
+        $result = $service->authenticate();
+
+        if ($result->isValid()) {
+            $class = new stdClass();
+            $class->username = $result->getIdentity();
+            $class->realm    = 'wrap';
+
+            return new Zend_Auth_Result(true, $class, array());
+        } else {
+            echo "Failed to authenticate!";
+        }
+    }
+
+    /**
+     * Setup this adapter to autoLogin
+     *
+     * @return boolean
+     */
+    public static function autoLogin()
+    {
+        return true;
+    }
+
+    /**
+     * Logs the user out by removing all the Shib cookies that are created.
+     *
+     */
+    public static function autoLogout()
+    {
+
+        foreach (array_keys($_COOKIE) as $name) {
+            if (preg_match('/^WRAP.*/', $name)) {
+
+                // Set the expiration date to one hour ago
+                setcookie($name, "", time() - 3600, "/", "ncsu.edu");
+            }
+        }
+    }
+
+    /**
+     * Flag to tell the app where the authenticaiton is managed
+     *
+     * @return boolean
+     */
+    public static function manageLocally()
+    {
+        return false;
+    }
+
+    /**
+     * flag to tell the app whether a user can sign up or not
+     *
+     * @return boolean
+     */
+    public static function allowUserSignUp()
+    {
+        return false;
+    }
+}
